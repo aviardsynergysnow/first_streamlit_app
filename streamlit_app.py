294616a05e1400500577b320672d9bd1e0018cf7
aviardsynergysnow <145585921+aviardsynergysnow@users.noreply.github.com>
2023-09-22 16:20:26 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit
+rimport streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -43,6 +43,6 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM fruit_load_list")
-my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load list contains :")
-streamlit.text(my_data_row)
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit load list contains :")
+streamlit.dataframe(my_data_rows)
